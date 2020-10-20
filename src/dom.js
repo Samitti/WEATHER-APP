1fc121867ef000a9d36c106646987a01924f22b3
Samitti <samuelteweldebrhan@gmail.com>
2020-10-20 21:39:19 +0300

    add render functionality to the index.html

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,13 @@
 const searchBtn = document.querySelector('#searchBtn');
 const cityName = document.querySelector('#cityName');
+const location = document.querySelector('#location');
+const temp = document.querySelector('#temp');
+const description = document.querySelector('#description');
 
 export {
   searchBtn,
   cityName,
+  location,
+  temp,
+  description,
 }
-
--- src/index.js
+++ src/index.js
@@ -2,38 +2,44 @@ import './style.css';
 import {
   searchBtn,
   cityName,
+  location,
+  temp,
+  description,
 } from './dom';
 
 const currentUnit = 'F';
 
 const getDatas = (weatherData) => {
   const city = weatherData.name;
-  const country = weatherData.sys.country;
+  const { country } = weatherData.sys;
   const temperature = weatherData.main.temp;
-  const description = weatherData.weather[0].description;  
-  
+  const { description } = weatherData.weather[0];
+
   return {
     city,
     country,
     temperature,
     description,
-  }
-}
+  };
+};
 
 const renderData = (selectedData) => {
-  
-}
+  location.textContent = `${selectedData.city},${selectedData.country}`;
+  temp.textContent = `${selectedData.temperature} \u00B0${currentUnit}`;
+  description.textContent = selectedData.description;
+};
 
 async function getWeather(location) {
-  alert("am In");
   const urlLink = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=4dc55b39eebd848607f246ce75cffecd`;
-  const response = await fetch(urlLink, {mode: 'cors'});
+  const response = await fetch(urlLink, { mode: 'cors' });
   const weatherData = await response.json();
   const selectedData = getDatas(weatherData);
-  console.log(selectedData);
+  renderData(selectedData);
 }
 
+getWeather('Asmara');
+
 searchBtn.onclick = (ev) => {
-  ev.preventDefault();  
+  ev.preventDefault();
   getWeather(cityName.value);
 };
